cb5c7d81846863c0863d57389c0372dd47695ceb
Udit Malhotra <uditmalhotra98@gmail.com>
2024-12-30 16:09:51 +0530

    route setting

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="w-full h-full ">
-        <BrowserRouter>
+        <BrowserRouter basename={"/udit-portfolio"}>
           <Routes>
             <Route index element={<Landing />} />
           </Routes>
