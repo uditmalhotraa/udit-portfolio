eed913e6ab0515d5900d07697c95ba8a1861e0a0
Udit Malhotra <48120948+uditmalhotraa@users.noreply.github.com>
2025-02-21 11:08:37 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export const Header = () => {
 
   return (
     <>
-      <div className="fixed flex flex-row justify-between items-center w-full h-16 px-4 shadow-container bg-cream/40 dark:bg-gray/40 backdrop-blur-lg z-20">
+      <div className="fixed flex flex-row justify-between items-center w-full h-16 px-4 shadow-container bg-cream/40 dark:bg-gray/40 backdrop-blur-lg z-20 border-b border-[#808080]">
         <a
           href="/"
           className=" font-poppins text-3xl font-semibold tracking-wider text-accent-light dark:text-accent-dark dark:text-accent-dark-dark"
