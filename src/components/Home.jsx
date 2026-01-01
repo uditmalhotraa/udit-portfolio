c26db68c4faae1395db261ee68c96fa365bba3b7
Udit Malhotra <uditmalhotra98@gmail.com>
2026-01-01 14:47:22 +0530

    fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
 
   return (
     <div className="flex flex-col w-full gap-12">
-      <div className="z-10 flex flex-col px-4 rounded-2xl !w-full h-auto border shadow-md border-black/10 dark:border-white/20 backdrop-blur-sm bg-cream dark:bg-black will-change-transform transform-gpu">
+      {/* <div className="z-10 flex flex-col px-4 rounded-2xl !w-full h-auto border shadow-md border-black/10 dark:border-white/20 backdrop-blur-sm bg-cream dark:bg-black will-change-transform transform-gpu">
         <div className="flex flex-row pt-4 gap-3 items-center lg:justify-between">
           <p className=" font-poppins text-3xl font-medium tracking-wide text-accent-light dark:text-accent-dark">
             Support My Work
@@ -39,7 +39,7 @@ export const Home = () => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="z-10 flex flex-col px-4 rounded-2xl !w-full h-auto border shadow-md border-black/10 dark:border-white/20 backdrop-blur-sm bg-cream dark:bg-black will-change-transform transform-gpu">
         <div className="flex flex-row mb-4 pt-4 gap-3 items-center ">
           <p className=" font-poppins text-3xl font-medium tracking-wide text-accent-light dark:text-accent-dark">
